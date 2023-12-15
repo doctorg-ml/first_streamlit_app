753096b3ffa2fde1923d60b9d9c7f97b3f9cf292
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 09:08:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Set title
 st.title('My Parents New Healthy Diner')
@@ -6,22 +7,26 @@ st.title('My Parents New Healthy Diner')
 # Breakfast Menu
 st.header('Breakfast Menu')
 
-st.text('🥣 Omega 3 & Blueberrey Oatmeal')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 # Another header for a special menu item
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Load the fruit macros data
+fruit_macros_url = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pd.read_csv(fruit_macros_url)
+
+# Set the index of the DataFrame to 'Fruit'
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+# Filter the DataFrame based on selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display the table on the page
+# Display the selected fruit macros in a table on the page
 st.dataframe(fruits_to_show)
-
-
