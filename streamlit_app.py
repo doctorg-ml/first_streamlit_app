17d3a24d6813e36ad98c107b014c96ae80880bda
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 08:38:06 -0700

    Update streamlit_app.py
    Added emoticons

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ st.title('My Parents New Healthy Diner')
 # Breakfast Menu
 st.header('Breakfast Menu')
 
-st.text('Omega 3 & Blueberrey Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text('🥣 Omega 3 & Blueberrey Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avodaco Toast')
+
+# Another header for a special menu item
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
