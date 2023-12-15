2bd219aef6595092fff77f8032839f1d594f8f65
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 08:27:58 -0700

    Update streamlit_app.py
    Added menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,18 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+# Set title
+st.title('My Parents New Healthy Diner')
+
+# Breakfast Menu
+st.header('Breakfast Menu')
+
+menu_items = {
+    'Avocado Toast': '$8.99',
+    'Greek Yogurt Parfait': '$6.99',
+    'Veggie Omelette': '$9.99',
+    'Fruit Bowl': '$5.99'
+}
+
+for item, price in menu_items.items():
+    st.write(f'{item}: {price}')
+
