e06d1e9bf2c01e5a4691b97ed56fa04dd97d0b67
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 08:46:17 -0700

    Update streamlit_app.py
    Added CSV fruit list.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,6 @@ st.text('🥑🍞 Avocado Toast')
 
 # Another header for a special menu item
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
