fa3a8c2dea9ce4e92acae3791eebe5f75ed2bd77
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 08:32:48 -0700

    Update streamlit_app.py
    Modified the code to be aligned with the instructions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,6 @@ st.title('My Parents New Healthy Diner')
 # Breakfast Menu
 st.header('Breakfast Menu')
 
-menu_items = {
-    'Avocado Toast': '$8.99',
-    'Greek Yogurt Parfait': '$6.99',
-    'Veggie Omelette': '$9.99',
-    'Fruit Bowl': '$5.99'
-}
-
-for item, price in menu_items.items():
-    st.write(f'{item}: {price}')
-
+st.text('Omega 3 & Blueberrey Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
