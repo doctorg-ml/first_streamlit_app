356d0dde49b3c7a996b3c74983fddeb8195da863
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-15 08:39:02 -0700

    Update streamlit_app.py
    Fixed avocado typo,

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.header('Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberrey Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avodaco Toast')
+st.text('🥑🍞 Avocado Toast')
 
 # Another header for a special menu item
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
