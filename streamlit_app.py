ef7b662f9e5f6450c58a13a2dc3e2022b632d1fc
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-19 16:17:20 -0700

    Update streamlit_app.py
    Improved the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,24 @@ import pandas as pd
 import requests as req
 
 # Set title
-st.title('My Parents New Healthy Diner')
+st.title('My Parents\' New Healthy Diner')
 
-# Breakfast Menu
+# Header for Breakfast Menu
 st.header('Breakfast Menu')
 
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avocado Toast')
+# List of breakfast items
+breakfast_items = [
+    '🥣 Omega 3 & Blueberry Oatmeal',
+    '🥗 Kale, Spinach & Rocket Smoothie',
+    '🐔 Hard-Boiled Free-Range Egg',
+    '🥑🍞 Avocado Toast'
+]
 
-# Another header for a special menu item
+# Display breakfast items
+for item in breakfast_items:
+    st.text(item)
+
+# Header for Build Your Own Fruit Smoothie
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Load the fruit macros data
@@ -21,24 +28,27 @@ fruit_macros_url = "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_
 my_fruit_list = pd.read_csv(fruit_macros_url)
 
 # Set the index of the DataFrame to 'Fruit'
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list.set_index('Fruit', inplace=True)
 
-# Let's put a pick list here so they can pick the fruit they want to include
+# Select fruits using multiselect
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # Filter the DataFrame based on selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the selected fruit macros in a table on the page
+# Display selected fruit macros in a table
 st.dataframe(fruits_to_show)
 
-# New Section to display fruityvice api response
+# Header for Fruityvice Fruit Advice
 st.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = req.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# Fetch data from Fruityvice API for a specific fruit (e.g., kiwi)
+fruit_name = "kiwi"
+fruityvice_response = req.get(f"https://fruityvice.com/api/fruit/{fruit_name}")
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+# Convert API response to a Pandas DataFrame for better display
+if fruityvice_response.status_code == 200:
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+else:
+    st.error(f"Failed to fetch data for {fruit_name}. Please try again later.")
