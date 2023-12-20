e26ac409e3c16928c04cfaff6581e3a6f7498f1f
Luis Gonzalez <73360941+doctorg-ml@users.noreply.github.com>
2023-12-19 19:02:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,22 @@ else:
     st.error(f"Failed to fetch data for {fruit_name}. Please try again later.")
 
 # Snowflake Connection
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+try:
+    snowflake_config = {
+        "user": st.secrets["snowflake"]["user"],
+        "password": st.secrets["snowflake"]["password"],
+        "account": st.secrets["snowflake"]["account"],
+        "warehouse": st.secrets["snowflake"]["warehouse"],
+        "database": st.secrets["snowflake"]["database"],
+        "schema": st.secrets["snowflake"]["schema"]
+    }
+
+    my_cnx = snowflake.connector.connect(**snowflake_config)
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_row = my_cur.fetchone()
+    st.text("Hello from Snowflake:")
+    st.text(my_data_row)
+
+except snowflake.connector.errors.ProgrammingError as e:
+    st.error(f"Snowflake Error: {e}")
